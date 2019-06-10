31d26252ad4efb8b7f6c050c96386655903a4ab4
Juan Ramirez <jramirez@interside.cl>
2019-06-10 14:18:01 -0400

    Se refactoriza para opotimizar funciones

--- app.js
+++ app.js
@@ -1,192 +1,114 @@
-let name = '';
-let respuestasCorrectas=0;
-let respuestasIncorrectas=0;
+    let name = '';
+    let respuestasCorrectas=0;
+    let respuestasIncorrectas=0;
+    // Lista de respuestas para cada idioma
+    let respuestasIngles=['house', 'dog', 'window'];
+    let respuestasFrances=['maison', 'chien', 'chat'];
 
-// Mensaje de bienvenida
-document.getElementById('bienvenida').style.display = 'none';
-document.getElementById("resultado").style.display = 'none';
-document.getElementById("alert-name").style.display = 'none';
-
-// Ocultar bloque preguntas en ingles
-document.getElementById('trivia-ingles').style.display = 'none';
-document.getElementById('question-one-i').style.display = 'none';
-document.getElementById('question-two-i').style.display = 'none';
-document.getElementById('question-three-i').style.display = 'none';
-document.getElementById('question1inglish').style.display = 'none';
-document.getElementById('question2inglish').style.display = 'none';
-document.getElementById('question3inglish').style.display = 'none';
-// Ocultar bloque preguntas en frances
-document.getElementById('trivia-frances').style.display = 'none';
-document.getElementById('question-one-p').style.display = 'none';
-document.getElementById('question-two-p').style.display = 'none';
-document.getElementById('question-three-p').style.display = 'none';
-document.getElementById('question1frances').style.display = 'none';
-document.getElementById('question2frances').style.display = 'none';
-document.getElementById('question3frances').style.display = 'none';
-
-
-// Evaluar si se ingreso el nombre
-function sub() {
-    name = document.getElementById("nombre").value;
-    if (name === '') {
-     document.getElementById('alert-name').style.display = 'block';
-    } else {
-        document.getElementById('inicio').style.display = 'none';
-        document.getElementById('bienvenida').style.display = 'block';
-        document.getElementById('saludo').innerHTML = `Hola ${name}!`;
+    // Funcion para simplificar la llamada de los IDs
+    // Recibe un nombre de id
+    function _id(id){
+        return document.getElementById(id);
     }
-}
 
-//preguntas de Ingles
-function triviaInglish() {
-    document.getElementById('bienvenida').style.display = 'none';
-    document.getElementById('trivia-ingles').style.display = 'block';
-    document.getElementById('question-one-i').style.display = 'block';
-}
+    // Funcion para mostrar u ocultar elementos
+    // Recibe un estado (none, block) y una lista de IDs a trabajar
+    function showHide(state, list){
+        list.forEach(element => {
+            _id(element).style.display=state;
+        });
+    }
 
-function evaluarPreguntaUnoI() {
-    let respuestaPreguntaUnoISeleccionada = document.querySelector('input[name=pregunta1]:checked');
-    
-    if(respuestaPreguntaUnoISeleccionada === null){
-        document.getElementById('question1inglish').style.display = 'block';
-    }else{
-        let respuestaPreguntaUnoIValor = document.querySelector('input[name=pregunta1]:checked').value;
-
-        if(respuestaPreguntaUnoIValor === 'house'){
-            respuestasCorrectas = respuestasCorrectas + 1;
+    // Reinicia la trivia del idioma seleccionado
+    function showTrivia(idioma){
+        respuestasCorrectas=0;
+        respuestasIncorrectas=0;
+        showHide('none',['bienvenida']);
+        if(idioma === 'ingles'){
+            showHide('block',['trivia-ingles', 'pregunta-ingles-1']);
         }else{
-            respuestasIncorrectas = respuestasIncorrectas + 1;
+            showHide('block',['trivia-frances', 'pregunta-frances-1']);
         }
-
-        document.getElementById('question-one-i').style.display = 'none';
-        document.getElementById('question-two-i').style.display = 'block';
     }
-}
 
-function evaluarPreguntaDosI() {
-    let respuestaPreguntaDosISeleccionada = document.querySelector('input[name=pregunta2]:checked');
-    
-    if(respuestaPreguntaDosISeleccionada === null){
-        document.getElementById('question2inglish').style.display = 'block';
-    }else{
-        let respuestaPreguntaDosIValor = document.querySelector('input[name=pregunta2]:checked').value;
+    // Muestra el bloque de resultado
+    function resultado() {
+        showHide('none',['pregunta-ingles-3', 'pregunta-frances-3']);
+        showHide('block',['resultado']);
+        _id('resultado-correctas').innerHTML=`Correctas ${respuestasCorrectas}`;
+        _id('resultado-incorrectas').innerHTML=`Incorrectas ${respuestasIncorrectas}`;
+    }
 
-        if(respuestaPreguntaDosIValor === 'dog'){
-            respuestasCorrectas = respuestasCorrectas + 1;
+    // Verifica si el checkbox esta seleccionado
+    function checked(item){
+        return document.querySelector(`input[name=pregunta${item}]:checked`) ? document.querySelector(`input[name=pregunta${item}]:checked`).value : null;
+    }
+
+    // Oculta todos los bloques que no son necesarios en un principio
+    showHide('none',[
+        'bienvenida', 'resultado', 'alert-name', 'alert-null',
+        'trivia-ingles', 'pregunta-ingles-1', 'pregunta-ingles-2', 'pregunta-ingles-3',
+        'trivia-frances', 'pregunta-frances-1', 'pregunta-frances-2', 'pregunta-frances-3'
+    ]);
+
+    // Evalua si se ingreso el nombre
+    function sub() {
+        name = _id('nombre').value;
+        if (name === '') {
+            showHide('block', ['alert-name']);
+        } else {
+            showHide('none', ['inicio']);
+            showHide('block', ['bienvenida']);
+            _id('saludo').innerHTML = `Hola ${name}!`;
+        }
+    }
+
+    // Verifica si la respuesta es correcta
+    // Rcibe el idioma y el checkbox
+    function evaluateQuestion(idioma, item){
+        if(checked(item) === null){
+            showHide('block', ['alert-null']);
         }else{
-            respuestasIncorrectas = respuestasIncorrectas + 1;
+            switch(idioma){
+                case 'ingles':
+                    if(checked(item) === respuestasIngles[item-1]){
+                        respuestasCorrectas++;
+                    }else{
+                        respuestasIncorrectas++;
+                    }
+                case 'frances':
+                    if(checked(item) === respuestasFrances[item-1]){
+                        respuestasCorrectas++;
+                    }else{
+                        respuestasIncorrectas++;
+                    }
+                default:
+                    break;
+            }
+            if(item<3){
+                showHide('none', [`pregunta-${idioma}-${item}`]);
+                showHide('block', [`pregunta-${idioma}-${item+1}`]);
+            }else{
+                resultado();
+            }
         }
-        document.getElementById('question-two-i').style.display = 'none';
-        document.getElementById('question-three-i').style.display = 'block';
     }
-}
-
-function evaluarPreguntaTresI() {
-    let respuestaPreguntaTresISeleccionada = document.querySelector('input[name=pregunta3]:checked');
     
-    if(respuestaPreguntaTresISeleccionada === null){
-        document.getElementById('question3inglish').style.display = 'block';
-    }else{
-        let respuestaPreguntaTresIValor = document.querySelector('input[name=pregunta3]:checked').value;
+    window.onload = function() {
+    // EJEMPLO EVENLISTENER
+    _id('guardar-nombre').addEventListener('click', sub);
 
-        if(respuestaPreguntaTresIValor === 'window'){
-            respuestasCorrectas = respuestasCorrectas + 1;
-        }else{
-            respuestasIncorrectas = respuestasIncorrectas + 1;
-        }
+    _id('btn-trivia-ingles').addEventListener('click', showTrivia('ingles'));
+    _id('btn-trivia-frances').addEventListener('click', showTrivia('frances'));
 
-        resultado();
-    }
+    _id('btn-reset-ingles').addEventListener('click', showTrivia('ingles'));
+    _id('btn-reset-frances').addEventListener('click', showTrivia('frances'));
+
+    _id('btn-pregunta-ingles-1').addEventListener('click', evaluateQuestion('ingles', 1));
+    _id('btn-pregunta-ingles-2').addEventListener('click', evaluateQuestion('ingles', 2));
+    _id('btn-pregunta-ingles-3').addEventListener('click', evaluateQuestion('ingles', 3));
+
+    _id('btn-pregunta-frances-1').addEventListener('click', evaluateQuestion('frances', 1));
+    _id('btn-pregunta-frances-2').addEventListener('click', evaluateQuestion('frances', 2));
+    _id('btn-pregunta-frances-3').addEventListener('click', evaluateQuestion('frances', 3));
 }
-
-//preguntas de frances
-function triviaFrances() {
-    document.getElementById('bienvenida').style.display = 'none';
-    document.getElementById('trivia-frances').style.display = 'block';
-    document.getElementById('question-one-p').style.display = 'block';
-}
-
-function evaluarPreguntaUnoP() {
-    let respuestaPreguntaUnoPSeleccionada = document.querySelector('input[name=pregunta1]:checked');
-    
-    if(respuestaPreguntaUnoPSeleccionada === null){
-        document.getElementById('question1frances').style.display = 'block';
-    }else{
-        let respuestaPreguntaUnoPValor = document.querySelector('input[name=pregunta1]:checked').value;
-
-        if(respuestaPreguntaUnoPValor === 'maison'){
-            respuestasCorrectas = respuestasCorrectas + 1;
-        }else{
-            respuestasIncorrectas = respuestasIncorrectas + 1;
-        }
-
-        document.getElementById('question-one-p').style.display = 'none';
-        document.getElementById('question-two-p').style.display = 'block';
-    }
-}
-
-function evaluarPreguntaDosP() {
-    let respuestaPreguntaDosPSeleccionada = document.querySelector('input[name=pregunta2]:checked');
-    
-    if(respuestaPreguntaDosPSeleccionada === null){
-        document.getElementById('question2frances').style.display = 'block';
-    }else{
-        let respuestaPreguntaDosPValor = document.querySelector('input[name=pregunta2]:checked').value;
-
-        if(respuestaPreguntaDosPValor === 'chien'){
-            respuestasCorrectas = respuestasCorrectas + 1;
-        }else{
-            respuestasIncorrectas = respuestasIncorrectas + 1;
-        }
-
-        document.getElementById('question-two-p').style.display = 'none';
-        document.getElementById('question-three-p').style.display = 'block';
-    }
-}
-
-function evaluarPreguntaTresP() {
-    let respuestaPreguntaTresPSeleccionada = document.querySelector('input[name=pregunta3]:checked');
-    
-    if(respuestaPreguntaTresPSeleccionada === null){
-        document.getElementById('question3frances').style.display = 'block';
-    }else{
-        let respuestaPreguntaTresPValor = document.querySelector('input[name=pregunta3]:checked').value;
-
-        if(respuestaPreguntaTresPValor === 'chat'){
-            respuestasCorrectas = respuestasCorrectas + 1;
-        }else{
-            respuestasIncorrectas = respuestasIncorrectas + 1;
-        }
-
-        resultado();
-    }
-}
-
-// Mostar resultado de trivia
-function resultado() {
-    document.getElementById('question-three-i').style.display = 'none';
-    document.getElementById('question-three-p').style.display = 'none';
-    document.getElementById('resultado').style.display = 'block';
-    document.getElementById('resultado-correctas').innerHTML=`Correctas ${respuestasCorrectas}`;
-    document.getElementById('resultado-incorrectas').innerHTML=`Incorrectas ${respuestasIncorrectas}`;
-}
-
-// botones para volver a empezar trivia
-function goToIngles() {
-    respuestasCorrectas=0;
-    respuestasIncorrectas=0;
-    document.getElementById('resultado').style.display = 'none';
-    triviaInglish();
-}
-
-function goToFrances() {
-    respuestasCorrectas=0;
-    respuestasIncorrectas=0;
-    document.getElementById('resultado').style.display = 'none';
-    triviaFrances()
-}
-
-// EJEMPLO EVENLISTENER
-
-let botonNombre=document.getElementById('guardar-nombre');
-botonNombre.addEventListener('click', sub);
